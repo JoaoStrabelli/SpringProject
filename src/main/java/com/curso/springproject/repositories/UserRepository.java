00023313329aba6b10f76bd884165bc807b9982f
JoaoStrabelli <joaopedro.dezanstrabelli@gmail.com>
2026-03-03 22:43:10 -0300

    Service layer, component registration

--- src/main/java/com/curso/springproject/repositories/UserRepository.java
+++ src/main/java/com/curso/springproject/repositories/UserRepository.java
@@ -2,7 +2,10 @@ package com.curso.springproject.repositories;
 
 import com.curso.springproject.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//Anotação opcional pois já está herdando um Repository
+@Repository
 public interface UserRepository extends JpaRepository <User, Long> {
 
 }
